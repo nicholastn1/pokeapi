0995f92557cca568e964e0d30b24b29b2dc29acf
nicholastn1 <nicholastimbo2022@gmail.com>
2023-09-09 14:21:51 -0300

    Configure to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,9 +17,11 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
